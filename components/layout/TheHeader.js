4c4099e952fa8fd8c20cafc21542ec3c736ea420
Dominik <dominik.rubroeder@icloud.com>
2022-05-05 16:34:20 +0200

    Style background of TheHeader

--- components/layout/TheHeader.js
+++ components/layout/TheHeader.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function TheHeader() {
   return (
-    <header className="fixed top-0 flex items-center justify-center p-4 w-full">
+    <header className="fixed top-0 flex items-center justify-center p-2 border-b border-b-white w-full bg-white/50 backdrop-blur z-10">
       <div className="relative w-32 h-8">
         <Image
           src="/images/logos/nintendo-logo-red.svg"
