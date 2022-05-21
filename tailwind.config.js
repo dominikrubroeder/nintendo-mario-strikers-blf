e6db9f614398802bc9fdce8fd046a3591f0da8f9
Dominik <dominik.rubroeder@icloud.com>
2022-05-21 16:24:54 +0200

    Add character colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,24 @@ module.exports = {
         'accent-dark': '#D10000',
         signal: '#FFB800',
         'signal-dark': '#F1980D',
-        mario: '#E60012',
-        'mario-dark': '#D10000',
-        luigi: '#04BE14',
-        'luigi-dark': '#038C0E',
+        mario: '#E0102F',
+        'mario-dark': '#C80E2A',
+        luigi: '#08A936',
+        'luigi-dark': '#07922F',
+        bowser: '#04BE14',
+        'bowser-dark': '#038C0E',
+        yoshi: '#70B921',
+        'yoshi-dark': '#64A41D',
+        'donkey-kong': '#983106',
+        'donkey-kong-dark': '#802905',
+        wario: '#F5DA0F',
+        'wario-dark': '#E1C809',
+        waluigi: '#5D2E8E',
+        'waluigi-dark': '#51287B',
+        peach: '#F096BE',
+        'peach-dark': '#ED7EAF',
+        daisy: '#EE830A',
+        'daisy-dark': '#D77709',
       },
     },
   },
@@ -24,6 +38,13 @@ module.exports = {
       addVariant('themed', '.themed &');
       addVariant('theme-mario', '.theme-mario &');
       addVariant('theme-luigi', '.theme-luigi &');
+      addVariant('theme-bowser', '.theme-bowser &');
+      addVariant('theme-yoshi', '.theme-yoshi &');
+      addVariant('theme-donkey-kong', '.theme-donkey-kong &');
+      addVariant('theme-wario', '.theme-wario &');
+      addVariant('theme-waluigi', '.theme-waluigi &');
+      addVariant('theme-peach', '.theme-peach &');
+      addVariant('theme-daisy', '.theme-daisy &');
     }),
   ],
 };
