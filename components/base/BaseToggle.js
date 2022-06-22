ffbc5cd88ad2429e2ac48d1ae03d29592a3df9a8
Dominik <dominik.rubroeder@icloud.com>
2022-06-22 12:52:30 +0200

    Add volume icons to interactive audio toggle component

--- components/base/BaseToggle.js
+++ components/base/BaseToggle.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { VolumeUpIcon, VolumeOffIcon } from '@heroicons/react/solid';
 
 const BaseToggle = (props) => {
   const [enabled, setEnabled] = useState(props.enabled || true);
@@ -17,22 +18,28 @@ const BaseToggle = (props) => {
 
   return (
     <div
-      className={`flex items-center gap-1 text-xs ${
+      className={`flex items-center gap-1 text-xs cursor-pointer ${
         enabled ? 'text-accent' : 'text-gray-400'
       }`}
+      onClick={toggle}
     >
       <span>{props.label}</span>
       <div
         className={`flex w-10 p-1 rounded-full bg-gray-100 transition-all ${
           enabled ? 'bg-accent' : ''
         }`}
-        onClick={toggle}
       >
         <span
-          className={`h-4 w-4 rounded-full  transition-all ${
+          className={`h-4 w-4 rounded-full flex items-center justify-center transition-all ${
             enabled ? 'bg-white translate-x-full' : 'bg-gray-300'
           }`}
-        ></span>
+        >
+          {enabled ? (
+            <VolumeUpIcon className="w-3 h-3"></VolumeUpIcon>
+          ) : (
+            <VolumeOffIcon className="w-3 h-3"></VolumeOffIcon>
+          )}
+        </span>
       </div>
     </div>
   );
