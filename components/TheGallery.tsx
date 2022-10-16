ce1e12bfec620f7c3fd10d25874174359ba03a21
Dominik <dominik.rubroeder@icloud.com>
2022-10-16 23:46:16 +0200

    Initialize creating custom slider

--- components/TheGallery.tsx
+++ components/TheGallery.tsx
@@ -1,4 +1,6 @@
-const galleryItems = [
+import { useCallback, useEffect, useState } from 'react';
+
+const galleryData = [
   {
     src: '/images/gallery/NSwitch_MarioStrikersBattleLeagueFootball_01.jpg',
   },
@@ -39,18 +41,56 @@ const galleryItems = [
 
 // Check quote slider animation on change: https://de.kickbase.com/
 export default function TheGallery() {
+  const [activeItem, setActiveItem] = useState(0);
+
+  // include multiple view options like sider, grid, list, ...
+
+  const calcValue = useCallback(
+    (index: number) => {
+      if (index > activeItem) return index * 100;
+      if (index < activeItem) return -(index * 100);
+
+      return 0;
+    },
+    [activeItem]
+  );
+
   return (
     <section className="p-4 overflow-x-auto whitespace-nowrap">
-      <div>
-        {galleryItems.map((galleryItem, index) => {
+      <div className="relative h-[75vh] md:h-[80vh]">
+        {galleryData.map((galleryItem, index) => {
           return (
             <div
               key={galleryItem.src}
-              style={{ backgroundImage: `url(${galleryItem.src})` }}
-              className="inline-block mr-4 w-[85vw] md:w-[95vw] h-[75vh] md:h-[80vh] bg-cover bg-center rounded-3xl"
+              style={{
+                backgroundImage: `url(${galleryItem.src})`,
+                transform: `translateX(${calcValue(index)}%)`,
+              }}
+              className={`absolute left-0 top-0 w-[85vw] h-full bg-cover bg-center rounded-3xl transition-all duration-1000 md:w-[95vw] md:h-[80vh] ${
+                index === activeItem ? 'scale-100' : 'scale-95'
+              }`}
             ></div>
           );
         })}
+      </div>
+
+      <div className="flex items-center p-8">
+        <ul className="flex items-center gap-4 mx-auto">
+          {galleryData.map((_, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                setActiveItem(index);
+              }}
+            >
+              <span
+                className={`block w-4 h-4 rounded-full themed:bg-themed-dark cursor-pointer transition-all hover:scale-150 ${
+                  index === activeItem ? 'bg-accent' : 'bg-accent/20'
+                }`}
+              ></span>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
