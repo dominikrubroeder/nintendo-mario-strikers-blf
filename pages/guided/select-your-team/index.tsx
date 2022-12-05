b38d7be37aa3bc50d76ab9890f1d8a0b651581a0
Dominik <dominik.rubroeder@icloud.com>
2022-12-05 21:38:39 +0100

    Implement Sticky Actions Bar Appearance animation for a better user experience

--- pages/guided/select-your-team/index.tsx
+++ pages/guided/select-your-team/index.tsx
@@ -1,6 +1,6 @@
 import { PlayIcon } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import CharacterSelection from '../../../components/character/CharacterSelection';
 import Layout from '../../../components/layout';
 import Heading from '../../../components/typography/Heading';
@@ -22,23 +22,31 @@ const SelectYourTeamPage: React.FC = () => {
   };
 
   return (
-    <Layout>
-      <section className="m-auto w-full max-w-screen-xl px-4">
-        <Heading className="headline--gradient">Wähle dein Team</Heading>
+    <>
+      <Layout>
+        <section className="m-auto w-full max-w-screen-xl px-4">
+          <Heading className="headline--gradient">Wähle dein Team</Heading>
 
-        <div className="animate--fadeUp mb-16 grid grid-cols-2 animation-delay-700">
-          <Button variant="contained" onClick={initRedirect}>
-            Weiter mit {character?.name}
-          </Button>
+          <div className="animate--fadeUp mb-16 grid grid-cols-2 animation-delay-700"></div>
 
-          <Button variant="text" onClick={initRedirect}>
-            Weiter ohne Team
-          </Button>
+          <CharacterSelection className="sm:grid-cols-2 lg:grid-cols-3" />
+        </section>
+      </Layout>
+
+      {character && (
+        <div className="animate--fadeUp fixed bottom-4 z-50 flex w-full items-center justify-center">
+          <div className="flex w-max items-center justify-center gap-2 rounded-full bg-accent-dark p-4">
+            <Button variant="contained" onClick={initRedirect}>
+              Weiter mit {character?.name}
+            </Button>
+
+            <Button variant="text" onClick={initRedirect}>
+              Weiter ohne Team
+            </Button>
+          </div>
         </div>
-
-        <CharacterSelection className="sm:grid-cols-2 lg:grid-cols-3" />
-      </section>
-    </Layout>
+      )}
+    </>
   );
 };
 
