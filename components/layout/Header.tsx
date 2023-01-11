36643450345fb27080c6d49b5623ba3090970a5b
Dominik <dominik.rubroeder@icloud.com>
2023-01-11 18:52:54 +0100

    Continue play audio on page switch

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -14,28 +14,23 @@ const Header: FC<HeaderProps> = ({ withBackButton = false }) => {
   const router = useRouter();
 
   return (
-    <>
-      <CurrentSound />
-      <CurrentSoundtrack />
+    <header className="relative z-40 flex h-20 w-full flex-wrap items-center justify-between p-4">
+      {withBackButton && (
+        <button onClick={() => router.back()}>
+          <ArrowLongLeftIcon className="h-4 w-4 text-accent themed:text-white" />
+        </button>
+      )}
 
-      <header className="relative z-40 flex h-20 w-full flex-wrap items-center justify-between p-4">
-        {withBackButton && (
-          <button onClick={() => router.back()}>
-            <ArrowLongLeftIcon className="h-4 w-4 text-accent themed:text-white" />
-          </button>
-        )}
-
-        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center transition active:scale-95">
-          <div className="relative flex flex-1 items-center justify-center self-center rounded-full p-2 themed:bg-accent-dark themed:text-white">
-            <Link href="/">
-              <a>
-                <Logo />
-              </a>
-            </Link>
-          </div>
+      <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center transition active:scale-95">
+        <div className="relative flex flex-1 items-center justify-center self-center rounded-full p-2 themed:bg-accent-dark themed:text-white">
+          <Link href="/">
+            <a>
+              <Logo />
+            </a>
+          </Link>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
