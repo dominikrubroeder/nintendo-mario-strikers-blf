a2f9e21e24e167d6acc1cdb383fb79e628d4662a
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-03-22 23:33:51 +0100

    Clean up CurrentSoundtrack

--- components/CurrentSoundtrack.tsx
+++ components/CurrentSoundtrack.tsx
@@ -1,36 +1,17 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext } from "react";
 import AudioContext from "../store/audioContext";
 import { soundtracks } from "../data/audio";
 
 const CurrentSoundtrack: React.FC = () => {
-  const audioRef = useRef<null | HTMLAudioElement>(null);
   const audioCtx = useContext(AudioContext);
-  const [mounted, setMounted] = useState(false);
 
-  // https://developer.chrome.com/blog/play-request-was-interrupted/
-
-  useEffect(() => {
-    setMounted(true);
-
-    if (
-      audioCtx?.soundtrack &&
-      audioRef.current &&
-      audioCtx.interactiveAudioisEnabled
-    ) {
-      audioRef.current.play();
-    }
-
-    return () => setMounted(false);
-  }, [audioCtx?.soundtrack, audioCtx?.interactiveAudioisEnabled]);
-
-  return mounted ? (
+  return (
     <audio
       id="currentSoundtrack"
-      ref={audioRef}
       src={audioCtx?.soundtrack ?? soundtracks[0].src}
       loop
     ></audio>
-  ) : null;
+  );
 };
 
 export default CurrentSoundtrack;
