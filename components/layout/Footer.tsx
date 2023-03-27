534b075931106903608d137abdcf06758fa8a5fd
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-03-27 14:04:53 +0200

    Add legal hint to footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className="px-4 py-12 text-center"></footer>;
+  return (
+    <footer className="px-4 py-8 text-center text-xs opacity-50">
+      Nintendo © {new Date().getFullYear()}, Developed by Dominik Rubröder.
+      University Project. All rights go to their respective owners.
+    </footer>
+  );
 };
 
 export default Footer;
