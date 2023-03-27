688913b0ce3057cba86c0bfde30a7ef3602158d2
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-03-27 14:04:01 +0200

    Place FloatingActionBar Desktop on top for better readability

--- components/FloatingActionBar.tsx
+++ components/FloatingActionBar.tsx
@@ -7,24 +7,28 @@ import PauseAudioButton from "./mini-audio-player/controls/PauseAudioButton";
 import { ArrowLongLeftIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import AppContext from "../store/appContext";
 
 interface FloatingActionBarProps {
   shouldBeVisible?: boolean;
 }
 
-const FloatingActionBar: FC<FloatingActionBarProps> = ({ shouldBeVisible }) => {
+const FloatingActionBar: FC<FloatingActionBarProps> = ({
+  shouldBeVisible = true,
+}) => {
+  const appCtx = useContext(AppContext);
   const audioCtx = useContext(AudioContext);
   const router = useRouter();
 
   return (
     <AnimatePresence>
-      {shouldBeVisible && (
+      {shouldBeVisible && !appCtx?.headerIsInView && (
         <motion.div
           key="actionBarWrapper"
-          className="fixed left-0 right-0 bottom-4 z-[100] flex w-full items-center justify-center gap-4 transition"
-          initial={{ y: 100 }}
+          className="fixed left-0 right-0 bottom-4 z-50 flex w-full items-center justify-center gap-4 transition lg:bottom-auto lg:top-4"
+          initial={{ y: -100 }}
           animate={{ y: 0 }}
-          exit={{ y: [-10, 100] }}
+          exit={{ y: [10, -100] }}
           transition={{
             ease: "easeInOut",
           }}
@@ -47,64 +51,60 @@ const FloatingActionBar: FC<FloatingActionBarProps> = ({ shouldBeVisible }) => {
               }}
             >
               <div className="flex items-center justify-center">
-                <AnimatePresence>
-                  <motion.div
-                    key="backButton"
-                    className="absolute -z-10 flex h-12 w-12 items-center justify-center rounded-full bg-accent text-white drop-shadow-lg themed:bg-accent-dark"
-                    initial={{ x: 0, scale: 0.3 }}
-                    animate={{ x: -128, scale: 1 }}
-                    exit={{ x: 0, scale: 0.3 }}
-                    transition={{
-                      type: "spring",
-                      damping: 16,
-                      stiffness: 400,
-                      delay: 1,
-                    }}
-                  >
-                    <div
-                      className="interactive flex items-center justify-center"
-                      onClick={() => router.back()}
-                    >
-                      <ArrowLongLeftIcon className="h-4 w-4 text-white" />
-                    </div>
-                  </motion.div>
-                </AnimatePresence>
-
-                <Button
-                  variant="plain"
-                  className="z-50 flex items-center gap-1 whitespace-nowrap bg-accent p-0 font-bold uppercase text-white themed:bg-signal"
-                  sound="coin"
-                  href="/buy-mario-strikers-battle-league-football"
+                <motion.div
+                  key="backButton"
+                  className="absolute -z-10 flex h-12 w-12 items-center justify-center rounded-full bg-accent text-white drop-shadow-lg themed:bg-accent-dark"
+                  animate={{ x: [0, -128], scale: [0.3, 1] }}
+                  exit={{ x: 0, scale: 0.3 }}
+                  transition={{
+                    type: "spring",
+                    damping: 16,
+                    stiffness: 400,
+                    delay: 1,
+                  }}
                 >
-                  <Image
-                    width={24}
-                    height={24}
-                    alt="Nintendo Mario Coin"
-                    src="/images/items/coin.png"
-                    className="object-contain"
-                  />
-                  Vorbestellen
-                </Button>
-
-                <AnimatePresence>
-                  <motion.div
-                    key="audioControls"
-                    className="absolute -z-10 flex h-12 w-12 items-center justify-center rounded-full bg-accent text-white drop-shadow-lg themed:bg-accent-dark"
-                    initial={{ x: 0, scale: 0.3 }}
-                    animate={{ x: 128, scale: 1 }}
-                    exit={{ x: 0, scale: 0.3 }}
-                    transition={{
-                      type: "spring",
-                      damping: 16,
-                      stiffness: 400,
-                      delay: 1,
-                    }}
+                  <div
+                    className="interactive flex items-center justify-center"
+                    onClick={() => router.back()}
                   >
-                    {!audioCtx?.isPlaying && <PlayAudioButton />}
+                    <ArrowLongLeftIcon className="h-4 w-4 text-white" />
+                  </div>
+                </motion.div>
 
-                    {audioCtx?.isPlaying && <PauseAudioButton />}
-                  </motion.div>
-                </AnimatePresence>
+                <motion.div exit={{ opacity: 0 }}>
+                  <Button
+                    variant="plain"
+                    className="z-50 flex items-center gap-1 whitespace-nowrap bg-accent p-0 font-bold uppercase text-white themed:bg-signal"
+                    sound="coin"
+                    href="/buy-mario-strikers-battle-league-football"
+                  >
+                    <Image
+                      width={24}
+                      height={24}
+                      alt="Nintendo Mario Coin"
+                      src="/images/items/coin.png"
+                      className="object-contain"
+                    />
+                    Vorbestellen
+                  </Button>
+                </motion.div>
+
+                <motion.div
+                  key="audioControls"
+                  className="absolute -z-10 flex h-12 w-12 items-center justify-center rounded-full bg-accent text-white drop-shadow-lg themed:bg-accent-dark"
+                  animate={{ x: [0, 128], scale: [0.3, 1] }}
+                  exit={{ x: 0, scale: 0.3 }}
+                  transition={{
+                    type: "spring",
+                    damping: 16,
+                    stiffness: 400,
+                    delay: 1,
+                  }}
+                >
+                  {!audioCtx?.isPlaying && <PlayAudioButton />}
+
+                  {audioCtx?.isPlaying && <PauseAudioButton />}
+                </motion.div>
               </div>
             </motion.div>
           </motion.div>
